31a6cbf2f9e5e8738173fe44b002b5839f696248
abhi1994j <abhibaban4@gmail.com>
2025-01-15 19:43:10 +0530

    update circle class style

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const toggle = document.getElementById('toggle-container');
 let circle = document.getElementById('circle');
 const day = document.getElementById('day');
 const night = document.getElementById('night');
-toggle.addEventListener('click', ()=>{
-   console.log("Hello world!");
-//    body.toggle('.dark-mode');
-   document.body.style.backgroundColor = 'black'
-   document.body.style.color = '#fff';
-   document.body.style.transition = 'background-color 1s, color 1s';  
-   circle.style.transform = 'translate(60px)';
-//    day.style.visibility = 'visisible';
+const body = document.body
+console.log(body.style);
+toggle.addEventListener('click', (e) => {
+    // console.log("Hello world!" , e);
+    document.body.classList.toggle('dark-mode');
+    circle.style.transform = 'translateX(calc(100% + 10px))';
+    document.body.style.transition = 'background-color 1s, color 1s';
+    // circle.style.transform = 'translate(60px)';
+    circle.style.transition = 'transform .5s'
+    // circle.style.overflow = 'hidden';
+    day.style.visibility = 'visisible';
 })
+
