1963c33a8ed5c12650ffa8332750dbf2a5d29e90
abhi1994j <abhibaban4@gmail.com>
2025-01-19 00:13:41 +0530

    finish dark mode

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const toggle = document.getElementById('toggle-container');
 let circle = document.getElementById('circle');
-const day = document.getElementById('day');
-const night = document.getElementById('night');
-const body = document.body
+const body = document.querySelector('body');
 console.log(body.style);
-toggle.addEventListener('click', (e) => {
+let h1 = document.querySelector('h1');
+
+let toggled = false;
+toggle.addEventListener('click', () => {
     // console.log("Hello world!" , e);
-    document.body.classList.toggle('dark-mode');
-    circle.style.transform = 'translateX(calc(100% + 10px))';
-    document.body.style.transition = 'background-color 1s, color 1s';
-    // circle.style.transform = 'translate(60px)';
-    circle.style.transition = 'transform .5s'
-    circle.style.overflow = 'hidden';
-    day.style.visibility = 'visisible';
+    if(!toggled){
+        h1.style.color = '#fff';
+        body.style.backgroundColor = 'black';
+        circle.classList.add('dark-mode');
+        toggled = true;
+    }
+    else{
+        h1.style.color = 'black';
+        body.style.backgroundColor = '#fff';
+        circle.classList.remove('dark-mode');
+        toggled = false;
+    }
+    // document.body.classList.toggle('dark-mode');
+    // circle.style.transform = 'translateX(calc(100% + 10px))';
+    // document.body.style.transition = 'background-color 1s, color 1s';
+    // // circle.style.transform = 'translate(60px)';
+    // circle.style.transition = 'transform .5s'
+    // circle.style.overflow = 'hidden';
+    // day.style.visibility = 'visisible';
 })
 
